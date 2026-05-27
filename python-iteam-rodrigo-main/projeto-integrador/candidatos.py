737220a43b22d318d7b9ded4c5a9b0d250ea2426
DrigoMatts <137211767+DrigoMatts@users.noreply.github.com>
2026-05-27 15:39:52 -0400

    Delete python-iteam-rodrigo-main directory
    deletando pasta

--- python-iteam-rodrigo-main/projeto-integrador/candidatos.py
+++
@@ -1,9 +0,0 @@
-class Candidato:
-    def __init__(self, nome, numero, partido):
-        self.nome = nome
-        self.numero = numero
-        self.partido = partido
-        self.votos = 0
-
-    def receber_voto(self):
-        self.votos += 1
--- python-iteam-rodrigo-main/projeto-integrador/relatorio.py
+++
@@ -1,4 +0,0 @@
-def exibir_resultado(candidatos):
-    print("\n--- RESULTADO DA VOTAÇÃO ---")
-    for c in candidatos.values():
-        print(f"{c.nome} ({c.partido}) - Número: {c.numero} - Votos: {c.votos}")
